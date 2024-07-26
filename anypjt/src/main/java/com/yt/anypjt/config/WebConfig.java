a5d06a83302dd1717bec073362a2365660adb35e
dev0tae <justdo0tae@gmail.com>
2024-07-26 11:48:03 +0900

    anypjt back source

---
+++ anypjt/src/main/java/com/yt/anypjt/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.yt.anypjt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+	/**
+	 * 스프링 부트 애플리케이션이 리액트 애플리케이션에서 오는 요청을 허용하도록 CORS를 설정
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		/*
+		 * allowCredentials(true) 쿠키 인증 요청 허용
+		 * allowedOrigins("*") 와 allowCredentials(true) 동시 사용 불가
+		 */		
+		registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+	}
+}
---
+++ anypjt/src/test/java/com/yt/anypjt/AnypjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yt.anypjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnypjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
