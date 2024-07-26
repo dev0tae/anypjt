05aeea96d4300890a08061447fb5fca9438bc6df
dev0tae <justdo0tae@gmail.com>
2024-07-26 11:55:17 +0900

    .

--- anypjt/src/main/java/com/yt/anypjt/AnypjtApplication.java
+++ anypjt/src/main/java/com/yt/anypjt/AnypjtApplication.java
@@ -3,6 +3,9 @@ package com.yt.anypjt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ */
 @SpringBootApplication
 public class AnypjtApplication {
 
